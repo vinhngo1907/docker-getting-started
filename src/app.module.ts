1416f66232a00d4e62e1b1af1855a75d2057c980
vinhngo1907 <vinhtrungngo1907@gmail.com>
2024-02-24 09:50:33 +0700

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {ConfigModule} from "@nestjs/config";
-// import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dbConfig } from './config/db';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(dbConfig)
-    // MongooseModule.forRoot(process.env.MONGO_URL,{
-    //   useNewUrlParser: true,
-    //   useFindAndModify: false,
-    //   useCreateIndex: true,
-    // })
-  ],
+  imports: [TypeOrmModule.forRoot(dbConfig), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
